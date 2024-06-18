ce796e822ca74a5cbaebeb3a1133b851a5b3c382
SalsaDeTomaquet <earlgreybaronai@gmail.com>
2024-06-18 17:10:02 +0200

    a

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -4,13 +4,13 @@ import Home from '../pages/Home';
 import Profile from '../pages/Profile';
 const Router = () => (
     <BrowserRouter>
-    <Routes>
-        <Route element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/tasks/" element={<Profile />} />
-            <Route path="*" element={<div>404</div>} />
-        </Route>
-    </Routes>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="/tasks/" element={<Profile />} />
+                <Route path="*" element={<div>404</div>} />
+            </Route>
+        </Routes>
     </BrowserRouter>
 );
 
