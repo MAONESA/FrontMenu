00d367898624ab0b5406b04f3aa5f33a813d7c2e
Sergi Treviño <itrevixx@gmail.com>
2024-06-18 19:10:16 +0200

    subida4

--- src/app/service/Event.js
+++ src/app/service/Event.js
@@ -3,3 +3,5 @@ const instance = axios.create({ baseURL: 'http://localhost:8080/event' });
 
 // READ
 export const getEvent = async () => await instance.get('/');
+
+export const getEventById = async (id) => await instance.get(`/detallesevento/${id}`);
