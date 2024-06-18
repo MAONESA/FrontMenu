5cca60b2101f4ae65c183f51ba259702feea01ed
SalsaDeTomaquet <earlgreybaronai@gmail.com>
2024-06-18 18:32:22 +0200

    Perfil

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,35 @@
+// import React from 'react';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card } from 'antd';
+import './profile.css';
+
+const { Meta } = Card;
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className='profile-container'>
+      <Card className='profile-card'
+        cover={
+          <img
+            alt="Imagen de perfil"
+            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          />
+        }
+        actions={[
+          <SettingOutlined key="setting" />,
+          <EditOutlined key="edit" />,
+          <EllipsisOutlined key="ellipsis" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+          title="Perfil"
+          description="This is the description"
+        />
+      </Card>
+    </div>
+
+  );
 };
 
 export default Profile;
