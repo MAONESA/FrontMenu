28eadb614617a366ad93e95723c573e059c77814
SalsaDeTomaquet <earlgreybaronai@gmail.com>
2024-06-18 19:19:53 +0200

    Perfil

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,35 +1,31 @@
 // import React from 'react';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import React from 'react';
+import { EditOutlined, EllipsisOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
 import { Avatar, Card } from 'antd';
-import './profile.css';
-
 const { Meta } = Card;
-
-const Profile = () => {
-  return (
-    <div className='profile-container'>
-      <Card className='profile-card'
-        cover={
-          <img
-            alt="Imagen de perfil"
-            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          />
-        }
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
-        ]}
-      >
-        <Meta
-          avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
-          title="Perfil"
-          description="This is the description"
+import './profile.css';
+const Profile = () => (
+  <div className='profile-container '>
+    <Card className='profile-card'
+      cover={
+        <img
+          alt="example"
+          //Esto es el banner
+          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" // Cambiar con la base de datos y ver como cambiarla sin de que error
         />
-      </Card>
-    </div>
-
-  );
-};
-
+      }
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <EllipsisOutlined key="ellipsis" />,
+      ]}
+    >
+      <Meta
+        avatar={<Avatar shape="square" icon={<UserOutlined />} />} // Perfil
+        title="Usuario" // Cambiar con la base de datos 
+        description="This is the description" // Poder modificar con editor y juntar con base de datos
+      />
+    </Card>
+  </div>
+);
 export default Profile;
