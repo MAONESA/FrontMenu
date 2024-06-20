91bf37118ca67b72db77303f3731b959413696d0
FerNumbers <seriouschocodrilo@gmail.com>
2024-06-20 16:29:31 +0200

    user.js

--- src/app/services/user.js
+++ src/app/services/user.js
@@ -4,6 +4,8 @@ import instance from './api';
 // READ
 export const getUser = async () => await instance.get('user/')
 
+export const getUserByName = async (nombre) => await instance.get(`user/${nombre}`)
+
 //CREATE;
 
 export const createUser = async (obj) => await instance.post('user/', obj)
